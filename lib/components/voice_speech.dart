b221b1a91acf94075cb88792c6b3f0f871f5b19e
Kuro Etele <kurogee@yahoo.com>
2020-01-04 10:44:29 +0100

    Text to speech Conversion Setup

---
+++ lib/components/voice_speech.dart
@@ -0,0 +1,21 @@
+import 'package:http/http.dart' as http;
+import 'package:snmobile/config.dart';
+
+Future<http.Response> getVoice(String text) async{
+
+  String url='https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=$VOICEAPI_KEY';
+  var body={
+      "audioConfig": {
+        "audioEncoding": "LINEAR16",
+        "pitch": 0,
+        "speakingRate": 1
+      },
+      "input": {
+        "text": "$text"
+      },
+      "voice": {
+        "languageCode": "en-US",
+        "name": "en-US-Wavenet-D"
+      }
+    };
+}
